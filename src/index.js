80e56e5c82693071a32a4de143fc99cfa8a06789
Tyler Klose <tylerklose@gmail.com>
2021-01-08 15:14:58 -0500

    Working scrollspy, but clicking links don't work

--- src/index.js
+++ src/index.js
@@ -2,5 +2,34 @@
 
 import "./css/main.css";
 
-// Say hello
-console.log("🦊 Hello! Edit me in src/index.js");
+import $ from 'jquery';
+import 'popper.js';
+import 'bootstrap';
+
+// $(document).ready(function() {
+//   console.log('setting up scrollspy')
+//   $('body').scrollspy({ target: '.scrollspy-navbar' });
+// 
+//   // Deactivate any, first, before we start up another
+//   // $(window).off('activate.bs.scrollspy');
+// 
+//   $(window).on('activate.bs.scrollspy', function (_, obj) {
+//     console.log('activate.bs.scrollspy');
+//     console.log('obj:', obj);
+//     console.log('obj.relatedTarget:', obj.relatedTarget);
+//     var ele = $('.scrollspy-navbar a[href="' + obj.relatedTarget + '"]');
+//     if(ele.length) {
+//       var offset = ele.offset().left;
+//       $('.scrollspy-navbar').animate({
+//         scrollLeft: offset - 30
+//       }, 300);
+//     }
+//   });
+// 
+//   $(window).on('scroll', function() {
+//     var ele = $('.scrollspy-navbar .nav-link.active');
+//     if(!ele.length) {
+//       $('.scrollspy-navbar .nav-link:first-child').addClass('active');
+//     }
+//   })
+// });
